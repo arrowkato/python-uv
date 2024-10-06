4e1d47f39277d9dcd8a34c2e12bf9de9d6fb1e32
shinji.sakaguchi <shinji.sakaguchi@mixi.co.jp>
2024-10-06 08:01:04 +0000

    add: add a simple unit test case.

---
+++ python_uv/calc.py
@@ -0,0 +1,14 @@
+def add(a: int, b: int) -> int:
+    """Add two numbers.
+
+    Args:
+    ----
+        a (int): First number.
+        b (int): Second number.
+
+    Returns:
+    -------
+        int: Sum of a and b.
+
+    """
+    return a + b
