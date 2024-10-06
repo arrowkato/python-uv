ee938deae787c97e895ab94894d376d6a44169b7
shinji.sakaguchi <shinji.sakaguchi@mixi.co.jp>
2024-10-06 08:46:56 +0000

    update: add type check in function

--- python_uv/calc.py
+++ python_uv/calc.py
@@ -6,9 +6,16 @@ def add(a: int, b: int) -> int:
         a (int): First number.
         b (int): Second number.
 
+    Raises:
+    ------
+        TypeError: Both a and b must be integers.
+
     Returns:
     -------
-        int: Sum of a and b.
+        int: sum of a and b.
 
     """
+    if not isinstance(a, int) or not isinstance(b, int):
+        err_msg = "Both a and b must be integers."
+        raise TypeError(err_msg)
     return a + b
